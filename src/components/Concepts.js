35f56adc8c715dbbe93aa2141422b5f8daad9719
Anderson Asprilla <anderson.asprilla@gmail.com>
2023-09-28 08:40:15 -0400

    Initial Commit

---
+++ src/components/Concepts.js
@@ -0,0 +1,25 @@
+import ConceptItem from "./ConceptItem";
+
+const Concepts = (props) => {
+  return (
+    <>
+      <ConceptItem
+        title={props.items[0].title}
+        image={props.items[0].image}
+        description={props.items[0].description}
+      />
+      <ConceptItem
+        title={props.items[1].title}
+        image={props.items[1].image}
+        description={props.items[1].description}
+      />
+      <ConceptItem
+        title={props.items[2].title}
+        image={props.items[2].image}
+        description={props.items[2].description}
+      />
+    </>
+  );
+};
+
+export default Concepts;
